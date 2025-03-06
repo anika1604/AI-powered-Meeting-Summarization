0f2fae0e272cfafb7a75a028baf66cf1458d96a7
dev_das <dasashmit16@gmail.com>
2025-03-06 20:21:21 +0530

    make transcriptions

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from dotenv import load_dotenv, dotenv_values
+from dotenv import load_dotenv
 from flask import Flask
 from groq import Groq
 
@@ -9,7 +9,33 @@ load_dotenv()
 my_api_key = os.getenv("GROQ_API_KEY")
 
 
+# Initialize the Groq client
 client = Groq(
     api_key=my_api_key,
 )
 
+
+
+# Specify the path to the audio file
+filename = os.path.dirname(__file__) + "/audio_samples/sample-0.mp3" # Replace with your audio file!
+
+
+folder_path = "transcription"
+if not os.path.exists(folder_path):
+    os.makedirs(folder_path)
+
+with open(filename, "rb") as file:
+    transcription = client.audio.transcriptions.create(
+        file=(filename, file.read()),
+        model="distil-whisper-large-v3-en",
+    #   prompt="Specify context or spelling",
+        response_format="verbose_json",
+    )
+    
+    transcript_text = transcription.text
+    
+    output_path = os.path.join(folder_path, "transcription_output.txt")
+    with open(output_path, "w") as output_file:
+        output_file.write(transcript_text)
+    
+    print(f"Transcription saved to '{output_path}'.")
