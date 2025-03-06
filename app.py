3356969ca15555a366d71900a3d4d89bd94fc950
dev_das <dasashmit16@gmail.com>
2025-03-07 00:30:46 +0530

    update flask app

--- app.py
+++ app.py
@@ -1,41 +1,19 @@
-import os
-from dotenv import load_dotenv
-from flask import Flask
-from groq import Groq
+from flask import Flask, render_template
+import json
+
+app = Flask(__name__)
+
+def read_json_file():
+    with open("json_responses/meeting_summary.json", "r") as file:
+        data = json.load(file)
+    return data
 
 
-load_dotenv()
-
-my_api_key = os.getenv("GROQ_API_KEY")
-
-
-# Initialize the Groq client
-client = Groq(
-    api_key=my_api_key,
-)
+@app.route('/')
+def home():
+    json_data = read_json_file()
+    return render_template("index.html", data=json_data)
 
 
-
-# Specify the path to the audio file
-filename = os.path.dirname(__file__) + "/audio_samples/sample-0.mp3" # Replace with your audio file!
-
-
-folder_path = "transcription"
-if not os.path.exists(folder_path):
-    os.makedirs(folder_path)
-
-with open(filename, "rb") as file:
-    transcription = client.audio.transcriptions.create(
-        file=(filename, file.read()),
-        model="distil-whisper-large-v3-en",
-    #   prompt="Specify context or spelling",
-        response_format="verbose_json",
-    )
-    
-    transcript_text = transcription.text
-    
-    output_path = os.path.join(folder_path, "transcription_output.txt")
-    with open(output_path, "w") as output_file:
-        output_file.write(transcript_text)
-    
-    print(f"Transcription saved to '{output_path}'.")
+if __name__ == '__main__':
+    app.run(debug=True)
